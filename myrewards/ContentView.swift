a76265a481a6d1866a516f1abf66416e4105d2d3
jackgaff <jackgaff@umich.edu>
2024-12-22 15:07:42 -0500

    Initial Commit

---
+++ myrewards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myrewards
+//
+//  Created by Jack Gaffney on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myrewards/myrewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myrewardsApp.swift
+//  myrewards
+//
+//  Created by Jack Gaffney on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct myrewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
